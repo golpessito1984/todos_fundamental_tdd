62d893cea8195b863bc4f67e73f1aacbcd5b1951
David Ruiz de la Rosa <david.ruizdelarosa@gmail.com>
2020-04-06 13:57:27 +0200

    User model

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,7 +2,7 @@ class TodosController < ApplicationController
   before_action :authenticate
 
   def index
-    @todos = Todo.where(email: current_email)
+    @todos = current_user.todos
   end
 
   def new
@@ -10,8 +10,7 @@ class TodosController < ApplicationController
   end
 
   def create
-    @todo = Todo.new(todo_params.merge(email: current_email))
-    if @todo.save
+    if current_user.todos.create(todo_params)
       redirect_to todos_path
     else
       render "new"
