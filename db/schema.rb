4fc7d085f50240f62f7000710bb5be6c623a82ff
David Ruiz de la Rosa <david.ruizdelarosa@gmail.com>
2020-04-06 11:59:16 +0200

    Add email todo table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_04_104019) do
+ActiveRecord::Schema.define(version: 2020_04_06_095556) do
 
   create_table "todos", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
   end
 
 end
